8f1e23d8d6b64f05e183216983d6383e80044412
MaabTaha <mgtaha22@gmail.com>
2026-03-23 12:03:22 -0600

    modifying server.js to match prof code

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+
 const app = express();
 
 // Middleware to parse JSON body
-app.use(express.text());
+// app.use(express.text());
+app.use(express.raw({ type: '*/*'}));
 
 // Echo endpoint
-app.post('/echo', (req, res) => {
+// app.post('/echo', (req, res) => {
+app.post('/', (req, res) => {
     // const message = req.body.message;
-    res.send(req.body);
+    // res.send(req.body);
+    const contentType = req.get('content-type') || 'text/plain';
+    res.set('Content-Type', contentType);
+    const body = req.body && req.body.length > 0 ? req.body: Buffer.from('');
+    res.send(body);
 
     // Send back the same string
     // res.send(message);
@@ -15,7 +22,8 @@ app.post('/echo', (req, res) => {
 
 // Port (Render requires process.env.PORT)
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
